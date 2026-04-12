8202e8879531418abb6ee2e33720bb7926ae3193
uzair khan <mduzairkhan4@gmail.com>
2026-04-12 11:42:09 +0530

    multiple ways to swap a number have shown in this

---
+++ swapNumbers/byUsingTumplePacking.py
@@ -0,0 +1,7 @@
+print("By using Tumple packing and tuple unpacking")
+a = 10
+b = 30
+print(a,b)
+## by using tuple packing and tuple unpacking
+a,b = b,a
+print(a,b)
---
+++ swapNumbers/takingTempVariable.py
@@ -0,0 +1,8 @@
+a = 6;
+b = 7;
+print (a,b)
+# swap
+c = a;
+a = b;
+b= c;
+print (a,b)
